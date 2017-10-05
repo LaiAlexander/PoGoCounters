d71d6d505a996fd86b70b5abfe4fa60789de98e1
Lai <laialexanderholmsen@gmail.com>
2017-10-05 21:11:52 +0200

    the script was very slow when writing the cell values to console. Solved this by using get_all_records(), which returns a list of dictionaries, containing all the information in the cells. This is much faster when I only need to read data, but it is not very suitable if you need to update cells in the sheet.

--- pogocounters.py
+++ pogocounters.py
@@ -1,19 +1,40 @@
-import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(os.environ.get('CREDS'), scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 
 gc = gspread.authorize(credentials)
 wks = gc.open("PoGo Raid Counters").sheet1
+print("Spreadsheet opened\n", flush=True)
 
 col1 = wks.col_values(1)
+col2 = wks.col_values(2)
+col3 = wks.col_values(3)
+print("Columns opened\n", flush=True)
 
-print("Spreadsheet opened\n")
-i = 1
+all_records = wks.get_all_records()
+print("All records have been fetched\n", flush=True)
+
+keys = []
+for key in all_records[1]:
+    keys.append(key)
+print(" | ".join(keys))
+#print(all_records[1].keys()) # not what I want
+for column in all_records:
+    print(column["Raid boss"], "|", column["Type"], "|", column["Weaknesses"])
+
+"""i = 0
 for value in col1:
-    if value:
-        print(wks.cell(i, 1).value + "   " + wks.cell(i, 2).value + "   " + wks.cell(i, 3).value)
-    i = i + 1
+    if not value:
+        break
+    #print(wks.cell(i, 1).value + "   " + wks.cell(i, 2).value + "   " + wks.cell(i, 3).value, flush=True)
+    #print(wks.cell(i, 1).value, flush=True) #seems a bit faster to print cells directly
+    # These are both slow because it makes 1+ API calls per iteration. 
+    print(col1[i] + " " + col2[i] + " " + col3[i], flush=True) # this is much faster, because it just accesses values stored in a list
+    i = i + 1"""
+
+print(wks.row_count)
+print(wks.col_count)
+print(wks.findall("Water"))
